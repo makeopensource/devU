283da5c76621f726a565c50676028301f2326edc
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2021-07-19 13:45:53 -0400

    Add assignment and course types (#5)
    * Added assignment types
    * Added course types
    
    Co-authored-by: Joey <j03ypoblete@gmail.com>
    Co-authored-by: Jesse Hartloff <hartloff@buffalo.edu>

--- devu-shared/src/index.ts
+++ devu-shared/src/index.ts
@@ -1,6 +1,8 @@
 // Shared Autograding types & utilities
 
+export * from './types/assignment.types'
 export * from './types/auth.types'
+export * from './types/course.types'
 export * from './types/user.types'
 export * from './types/validation.types'
 
---
+++ devu-shared/src/types/assignment.types.ts
@@ -0,0 +1,30 @@
+export type Assignment = {
+  id?: number
+  courseId: number
+  name: string
+  startDate: string
+  dueDate: string
+  endDate: string
+  gradingType: GradingType
+  categoryName: string
+  description: string | null
+  maxFileSize: number
+  maxSubmissions: number | null
+  disableHandins: boolean
+  createdAt?: string
+  updatedAt?: string
+}
+
+export type GradingTypeDetail = {
+  label: string
+  value: string
+}
+
+export const gradingTypeDetails: GradingTypeDetail[] = [
+  { label: 'Autograded - Code', value: 'code' },
+  { label: 'Autograded - Not Code', value: 'non-code' },
+  { label: 'Manually Graded', value: 'manual' },
+]
+
+export const gradingTypes = ['code', 'non-code', 'manual'] as const
+export type GradingType = typeof gradingTypes[number]
