778853227646ba7975cdf00b984ec5a4a4d99955
Michael Drewitt <mikedrewitt@gmail.com>
2021-08-18 13:00:53 -0400

    My submissions (#40)
    * Submissions list page
    * Fetching assignment and course info to display all details of a submission
    * Created submission list item
    * Created generic list item from what we already had
    * Refactored a bit to use generic list item

--- devU-client/src/components/listItems/userCourseListItem.tsx
+++ devU-client/src/components/listItems/userCourseListItem.tsx
@@ -1,38 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import { UserCourse, Course } from 'devu-shared-modules'
+
+import ListItemWrapper from 'components/shared/layouts/listItemWrapper'
 
 import { prettyPrintDate } from 'utils/date.utils'
 
 import styles from './userCourseListItem.scss'
-
-import ColorHash from 'color-hash'
 
 type Props = {
   userCourse: UserCourse
   course: Course
 }
 
-const colorHash = (input: string) => {
-  const hash = new ColorHash({ hue: { min: 90, max: 270 } })
-  return hash.hex(input)
-}
-
 const UserCourseListItem = ({ course }: Props) => (
-  <Link to={`/courses/${course.id}`} className={styles.container}>
-    <div className={styles.tag} style={{ backgroundColor: colorHash(course.number) }}></div>
-    <div>
-      <div className={styles.name}>{course.name}</div>
-      <div className={styles.subText}>
-        <div>{course.number}</div>
-        <div>Semester: {course.semester}</div>
-        <div>Start Date: {prettyPrintDate(course.startDate)}</div>
-        <div>End Date: {prettyPrintDate(course.endDate)}</div>
+  <ListItemWrapper to={`/courses/${course.id}`} tag={course.number}>
+    <div className={styles.name}>{course.name}</div>
+    <div className={styles.subText}>
+      <div>{course.number}</div>
+      <div>Semester: {course.semester}</div>
+      <div>Start Date: {prettyPrintDate(course.startDate)}</div>
+      <div>End Date: {prettyPrintDate(course.endDate)}</div>
 
-        {/* Add any other class information here */}
-      </div>
+      {/* Add any other class information here */}
     </div>
-  </Link>
+  </ListItemWrapper>
 )
 
 export default UserCourseListItem
---
+++ devU-client/src/components/shared/layouts/listItemWrapper.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './listItemWrapper.scss'
+
+import ColorHash from 'color-hash'
+
+type Props = {
+  to: string
+  children: React.ReactNode
+  tag?: string
+  className?: string
+}
+
+const colorHash = (input: string) => {
+  const hash = new ColorHash({ hue: { min: 90, max: 270 } })
+
+  return hash.hex(input)
+}
+
+const ListItemWrapper = ({ to, children, tag, className = '' }: Props) => (
+  <Link to={to} className={styles.container}>
+    {tag && <div className={styles.tag} style={{ backgroundColor: colorHash(tag) }}></div>}
+    <div className={className}>{children}</div>
+  </Link>
+)
+
+export default ListItemWrapper
