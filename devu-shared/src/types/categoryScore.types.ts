db33deee20d1042557af1db6fbbfba2996e14992
Alan Soto <alansoto1053@yahoo.com>
2023-09-14 18:53:05 -0400

    Add Category Type and Removed Letter Grade

--- devu-shared/src/types/categoryScore.types.ts
+++ devu-shared/src/types/categoryScore.types.ts
@@ -4,30 +4,6 @@ export type CategoryScore = {
     courseId: number
     category: string
     score: number | null
-    letterGrade: LetterGrade
     createdAt?: string
     updatedAt?: string
 }
-
-export const letterGrades = [
-    'A+',
-    'A', 
-    'A-', 
-    'B+', 
-    'B', 
-    'B-', 
-    'C+', 
-    'C',
-    'C-',
-    'D+',
-    'D',
-    'D-',
-    'F',
-    '>F<',
-    'FX',
-    'I',
-    'S',
-    'U',
-    'W',
-] as const
-export type LetterGrade = typeof letterGrades[number]
