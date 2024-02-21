c6f038dc6a55a3c8f209dc0f3600ea83377ea7cd
RA341 <razerishi1505@gmail.com>
2024-02-21 15:43:32 -0500

    updated ncag grader for regex

--- devU-api/src/entities/nonContainerAutoGrader/nonContainerAutoGrader.grader.ts
+++ devU-api/src/entities/nonContainerAutoGrader/nonContainerAutoGrader.grader.ts
@@ -1,8 +1,23 @@
 import { NonContainerAutoGrader } from 'devu-shared-modules'
 
 export async function checkAnswer(studentAnswer: string, nonContainerAutoGrader: NonContainerAutoGrader) {
-  if (studentAnswer === nonContainerAutoGrader.correctString) {
-    return nonContainerAutoGrader.score
+
+  if (nonContainerAutoGrader.isRegex) {
+    // Create a regex pattern
+    const pattern: RegExp = new RegExp(nonContainerAutoGrader.correctString)
+    // Use the test method
+    const isMatch: boolean = pattern.test(studentAnswer)
+    if (isMatch) {
+      return nonContainerAutoGrader.score
+    }
+  } else {
+    // if no regex is set use normal string matching
+    if (studentAnswer === nonContainerAutoGrader.correctString) {
+      return nonContainerAutoGrader.score
+    }
   }
-  return null
+
+  // default value to return if all conditions fail to execute
+  // i.e. student has the incorrect answer or improperly formatted
+  return 0
 }
