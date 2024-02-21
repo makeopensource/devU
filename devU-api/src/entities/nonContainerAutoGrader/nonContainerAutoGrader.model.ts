f1ccb05add6bdec6b4284653bf751abb1af98a0a
RA341 <razerishi1505@gmail.com>
2024-02-21 15:14:42 -0500

    added isRegex field type

--- devU-api/src/entities/nonContainerAutoGrader/nonContainerAutoGrader.model.ts
+++ devU-api/src/entities/nonContainerAutoGrader/nonContainerAutoGrader.model.ts
@@ -1,40 +1,43 @@
 import {
-    Column,
-    CreateDateColumn,
-    DeleteDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm'
 import AssignmentModel from '../assignment/assignment.model'
 
 @Entity({ name: 'nonContainerAutoGrader' })
 export default class NonContainerAutoGraderModel {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date
 
-    @UpdateDateColumn({ name: 'updated_at' })
-    updatedAt: Date
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date
 
-    @DeleteDateColumn({ name: 'deleted_at' })
-    deletedAt?: Date
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date
 
-    @Column({ name: 'assignment_id' })
-    @JoinColumn({ name: 'assignment_id' })
-    @ManyToOne(() => AssignmentModel)
-    assignmentId: number
+  @Column({ name: 'assignment_id' })
+  @JoinColumn({ name: 'assignment_id' })
+  @ManyToOne(() => AssignmentModel)
+  assignmentId: number
 
-    @Column({name:'question', length: 128 })
-    question: string
+  @Column({ name: 'question', length: 128 })
+  question: string
 
-    @Column({name:'score'})
-    score: number
+  @Column({ name: 'score' })
+  score: number
 
-    @Column({ name:'correct_string', length: 128 })
-    correctString: string
+  @Column({ name: 'is_regex' })
+  isRegex: boolean
+
+  @Column({ name: 'correct_string', length: 128 })
+  correctString: string
 }
