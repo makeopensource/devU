938003d642f372eebc21ff458f0c75d7b80d2b6e
RA341 <razerishi1505@gmail.com>
2024-03-02 16:06:40 -0500

    added back code after merge

---
+++ devu-shared/src/types/deadlineExtensions.types.ts
@@ -0,0 +1,11 @@
+export type DeadlineExtensionsTypes = {
+  id?: number
+  creatorId: number
+  userId: number
+  assignmentId: number
+  deadlineDate: string
+  newStartDate?: string
+  newEndDate?: string
+  createdAt?: string
+  updatedAt?: string
+}
