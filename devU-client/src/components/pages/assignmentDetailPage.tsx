b3950b213a11f882b24870869e46f3f50d5b9733
keiferms3 <keiferms3@gmail.com>
2024-03-28 21:26:32 -0400

    assignmentDetailPage now displays submissions

--- devU-client/src/components/pages/assignmentDetailPage.tsx
+++ devU-client/src/components/pages/assignmentDetailPage.tsx
@@ -1,7 +1,7 @@
 import React,{ useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import PageWrapper from 'components/shared/layouts/pageWrapper'
-import { AssignmentProblem /*, ExpressValidationError*/ } from 'devu-shared-modules'
+import { AssignmentProblem, Submission, SubmissionScore, SubmissionProblemScore } from 'devu-shared-modules' 
 import RequestService from 'services/request.service'
 import ErrorPage from './errorPage'
 import LoadingOverlay from 'components/shared/loaders/loadingOverlay'
@@ -22,20 +22,39 @@ const AssignmentDetailPage = () => {
     const [loading, setLoading] = useState(true)
     const [formData, setFormData] = useState({})
     const [assignmentProblems, setAssignmentProblems] = useState(new Array<AssignmentProblem>())
+    const [submissions, setSubmissions] = useState(new Array<Submission>())
+    const [submissionScores, setSubmissionScores] = useState(new Array<SubmissionScore>())
+    const [submissionProblemScores, setSubmissionProblemScores] = useState(new Array<SubmissionProblemScore>())
 
     
     useEffect(() => {
           fetchData()
     }, []);
 
-    // useEffect(() => {
-    //     console.log(submissionResponse);
-    // }, [submissionResponse]);
 
     const fetchData = async () => {
          try {
-             const data = await RequestService.get(`/api/assignment-problems/${assignmentId}`)
-             setAssignmentProblems(data)
+             const assignmentProblemsReq = await RequestService.get<AssignmentProblem[]>(`/api/assignment-problems/${assignmentId}`)
+             setAssignmentProblems(assignmentProblemsReq)
+
+             const submissionsReq = await RequestService.get<Submission[]>(`/api/submissions?assignment=${assignmentId}&user=${userId}`)
+             submissionsReq.sort((a, b) => (Date.parse(b.createdAt ?? '') - Date.parse(a.createdAt ?? '')))
+             setSubmissions(submissionsReq)
+
+             const submissionScoresPromises = submissionsReq.map(s => {
+                return RequestService.get<SubmissionScore[]>(`/api/submission-scores?submission=${s.id}`)
+             })
+             const submissionScoresReq = (await Promise.all(submissionScoresPromises)).reduce((a, b) => a.concat(b), [])
+             setSubmissionScores(submissionScoresReq)
+
+             const submissionProblemScoresPromises = submissionsReq.map(s => {
+                return RequestService.get<SubmissionProblemScore[]>(`/api/submission-problem-scores/${s.id}`)
+             })
+             const submissionProblemScoresReq = (await Promise.all(submissionProblemScoresPromises)).reduce((a, b) => a.concat(b), [])
+             setSubmissionProblemScores(submissionProblemScoresReq)
+
+                
+             
          }catch(error){
              setError(error)
          }finally{
@@ -85,16 +104,38 @@ const AssignmentDetailPage = () => {
     return(
         <PageWrapper>
             <Link to = {`/courses/${courseId}/assignments/${assignmentId}/update`} className = {styles.button}>Update Assignment</Link>
-            <br></br><br></br><br></br><br></br>
+            <br/><br/><br/>
             <Link to = {`/ncagtest`} className = {styles.button}>Add Non-Container Auto-Graders</Link>
             <h1>Assignment Detail</h1>
             {assignmentProblems.map(assignmentProblem => (
                 <div>
-                    <h1>{assignmentProblem.problemName}</h1>
+                    <h2>{assignmentProblem.problemName}</h2>
                     <TextField id={assignmentProblem.problemName} label='Answer' onChange={handleChange} />
                 </div>
             ))}
             <Button onClick={handleSubmit}>Submit</Button>
+            <br/>
+            {console.log(submissionProblemScores)}
+            {console.log(submissionScores)}
+            {submissions.map((s, index) => (
+                <div>
+                    <h2>Submission {submissions.length - (index)}:</h2>
+                    <Link to = {`/submissions/${s.id}`} className = {styles.button}>View Submission Details</Link>
+                    <br/><br/>
+                    <table>
+                        {assignmentProblems.map(ap => (
+                            <th>{ap.problemName} ({ap.maxScore})</th>
+                        ))}
+                        <th>Total Score</th>
+                        <tr>
+                            {assignmentProblems.map(ap => (
+                                <td>{submissionProblemScores.find(sps => (sps.assignmentProblemId === ap.id && sps.submissionId === s.id))?.score ?? "N/A"}</td>
+                            ))}
+                            <td>{submissionScores.find(ss => ss.submissionId === s.id)?.score ?? "N/A"}</td>
+                        </tr>
+                    </table> <br/>
+                </div>
+            ))}
         </PageWrapper>
     )
 }
