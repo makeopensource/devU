63f65b0fdd09474aac558cc99ec6d513b7adf4a4
Kevin Zhong <112502339+kevinzhong930@users.noreply.github.com>
2024-03-29 03:55:48 -0400

    Added manual grading to frontend
    Added manual grading to the frontend. There is a bug where the api doesn't create the releasedAt date when updating a submission score entity so I hardcoded a date.

--- devU-client/src/components/pages/submissionDetailPage.tsx
+++ devU-client/src/components/pages/submissionDetailPage.tsx
@@ -3,18 +3,28 @@ import React,{useState,useEffect} from 'react'
 import PageWrapper from 'components/shared/layouts/pageWrapper'
 import RequestService from 'services/request.service'
 import { SubmissionScore } from 'devu-shared-modules'
+import Button from 'components/shared/inputs/button'
+import TextField from 'components/shared/inputs/textField'
+import { useActionless } from 'redux/hooks'
+import { SET_ALERT } from 'redux/types/active.types'
 
 const SubmissionDetailPage = (props : any) => { 
     const { state } = props.location
+    const [setAlert] = useActionless(SET_ALERT)
     const [submissionScore, setSubmissionScore] = useState<SubmissionScore | null>(null)
+    const [showManualGrade, setToggleManualGrade] = useState<boolean>(false)
+    const [formData, setFormData] = useState({
+        submissionId: state.id,
+        score: 0,
+        feedback: '',
+        releasedAt: "2024-10-05T14:48:00.000Z",
+    })
 
     const fetchData = async () => {
         try {
             const data = await RequestService.get<SubmissionScore>(
                 `/api/submission-scores/${state.id}`
             )
-            console.log(data)
-            console.log(typeof(data))
             setSubmissionScore(data)
         } catch (error) {
             console.log('Error fetching submission problem scores', error)
@@ -25,9 +35,49 @@ const SubmissionDetailPage = (props : any) => {
         fetchData()
     }, [])
 
+    const handleClick = () => {
+        setToggleManualGrade(!showManualGrade)
+    }
+
+    const handleChange = (value : string, e : React.ChangeEvent<HTMLInputElement>) => {
+        const key = e.target.id
+        setFormData(prevState => ({...prevState,[key] : value}))
+    }
+
+    const handleManualGrade = async () => {
+        if (submissionScore) {
+            // Update the submission score
+            console.log('Submission Exists')
+            await RequestService.put( `/api/submission-scores/${submissionScore.id}`, formData)
+            .then(() => {
+                setAlert({ autoDelete: true, type: 'success', message: 'Submission Score Updated' })
+            
+            })
+        }
+        else {
+            // Create a new submission score
+            console.log('No Submission')
+            await RequestService.post( `/api/submission-scores`, formData)
+            .then(() => {
+                setAlert({ autoDelete: true, type: 'success', message: 'Submission Score Created' })
+            })
+        }
+    }
+
     return(
         <PageWrapper>
             <h1>Submission Detail</h1>
+
+            <Button onClick={handleClick}>Manually Grade</Button>
+
+            {showManualGrade && (
+                <div>
+                    <TextField id="score" placeholder="Score" onChange={handleChange}/>
+                    <TextField id="feedback" placeholder="Feedback" onChange={handleChange}/>
+                    <Button onClick={handleManualGrade}>Submit</Button>
+                </div>
+            )}
+
             {submissionScore ? (
                 <div>
                     <h1>{submissionScore.submissionId}</h1>
@@ -35,7 +85,9 @@ const SubmissionDetailPage = (props : any) => {
                     <h3>{submissionScore.feedback}</h3>
                     <br></br>
                 </div>
-            ) : null}
+            ) : (
+                <h1>No submission score/feedback available</h1>
+            )}
         </PageWrapper>
     )
 }
