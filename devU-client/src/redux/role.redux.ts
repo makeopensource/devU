564f478914dfe6299bceb2c485496574ad9cc1a8
RA341 <razerishi1505@gmail.com>
2024-04-18 02:27:17 -0400

    added toggle in creating courses and assignments buttons
    Also added isInstructor function to roletype

--- devU-client/src/redux/role.redux.ts
+++ devU-client/src/redux/role.redux.ts
@@ -11,31 +11,35 @@ type UserActionTypes = UpdateUserRoleAction;
 export const updateUserRole = (newRole: string): UpdateUserRoleAction => ({
   type: 'UPDATE_USER_ROLE',
   payload: newRole,
-});
+})
 
 // Define interface for the initial state
 interface UserState {
   userRole: string;
+  isInstructor: () => boolean
 }
 
 // Load initial state from localStorage if available
 const initialState: UserState = {
   userRole: localStorage.getItem('userRole') || 'Student', // Default role is 'student'
-};
+  isInstructor: function() {
+    return this.userRole === 'Instructor';
+  },
+}
 
 // Reducer
 const reducer = (state: UserState = initialState, action: UserActionTypes): UserState => {
   switch (action.type) {
     case 'UPDATE_USER_ROLE':
       // Save to localStorage when role is updated
-      localStorage.setItem('userRole', action.payload);
+      localStorage.setItem('userRole', action.payload)
       return {
         ...state,
         userRole: action.payload,
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default reducer;
+export default reducer
