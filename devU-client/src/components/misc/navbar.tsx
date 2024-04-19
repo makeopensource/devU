18011eebf5bb309a5d03a09929468bf24ade4d6e
keiferms3 <keiferms3@gmail.com>
2024-04-18 23:38:40 -0400

    Navbar functional, though unloaded route details are visible for a moment, unsure how to fix

--- devU-client/src/components/misc/navbar.tsx
+++ devU-client/src/components/misc/navbar.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
 import { Link } from 'react-router-dom'
 import styles from './navbar.scss'
+import RequestService from 'services/request.service'
+import { Assignment, Course, User } from 'devu-shared-modules'
+
+const UserBreadcrumb = ({ match }: any) => {
+    const [userName, setUserName] = useState('')
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    const getUser = async () => { 
+        const user = await RequestService.get<User>( `/api/users/${match.params.userId}` ) 
+        setUserName(user.externalId)
+    }
+
+    return (<>{userName}</>)
+}
+
+const CourseBreadcrumb = ({ match }: any) => {
+    const [courseName, setCourseName] = useState('')
+
+    useEffect(() => {
+        getCourse()
+    }, [])
+
+    const getCourse = async () => { 
+        const course = await RequestService.get<Course>( `/api/courses/${match.params.courseId}` ) 
+        setCourseName(course.name)
+    }
+
+    return (<>{courseName}</>)
+}
+
+const AssignmentBreadcrumb = ({ match }: any) => {
+    const [assignmentName, setAssignmentName] = useState('')
+
+    useEffect(() => {
+        getAssignment()
+    }, [])
+
+    const getAssignment = async () => { 
+        const assignment = await RequestService.get<Assignment>( `/api/assignments/${match.params.assignmentId}` ) 
+        setAssignmentName(assignment.name)
+    }
+
+    return (<>{assignmentName}</>)
+}
+
+const routes = [
+    { path: '/', breadcrumb: 'Home' },
+    { path: '/users/:userId', breadcrumb: UserBreadcrumb},
+    { path: '/courses/:courseId', breadcrumb: CourseBreadcrumb},
+    { path: '/courses/:courseId/assignments/:assignmentId', breadcrumb: AssignmentBreadcrumb},
+]
 
 const Navbar = ({breadcrumbs}: any) => {
 
@@ -18,4 +72,4 @@ const Navbar = ({breadcrumbs}: any) => {
         
 }
 
-export default withBreadcrumbs()(Navbar)
+export default withBreadcrumbs(routes, { disableDefaults: true })(Navbar)
