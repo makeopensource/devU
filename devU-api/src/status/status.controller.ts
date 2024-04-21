15442c6dda47cf2381fd1242cb60449de6266bd9
RA341 <razerishi1505@gmail.com>
2024-04-21 00:26:56 -0400

    added tango status

--- devU-api/src/status/status.controller.ts
+++ devU-api/src/status/status.controller.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
+import { getInfo, tangoHelloWorld } from '../tango/tango.service'
 
 export async function get(req: Request, res: Response, next: NextFunction) {
-  res.status(200).send()
+  let tango = false
+  let tangoStatus;
+  try {
+    tango = await tangoHelloWorld()
+    tangoStatus = await getInfo()
+    console.log(tangoStatus)
+  } catch (e) {
+    console.log('failed to start tango')
+    console.log(e)
+  }
+  res.status(200).send(`Api is online.\n Tango is ${tango ? `Online` : 'Offline'}`)
 }
