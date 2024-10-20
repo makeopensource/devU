c598cafef622a05548a29b05f86f4ed0fd65cb1c
RA341 <razerishi1505@gmail.com>
2024-10-19 23:54:56 -0400

    added checks for max file size and max submissions

---
+++ devU-api/src/entities/submission/submission.middleware.ts
@@ -0,0 +1,61 @@
+// middleware to enforce mac submissions
+
+import { Request, Response, NextFunction } from 'express'
+import SubmissionService from './submission.service'
+import AssignmentService from '../assignment/assignment.service'
+
+// TODO discuss how to bypass this when an instructor wants to eg bypass for a specific student
+async function checkSubmissions(req: Request, res: Response, next: NextFunction) {
+  const body = req.body
+  const userID = req.currentUser?.userId
+  const assignmentId = body.assignmentId
+
+  if (!userID) {
+    return res.status(403).send('userid is missing')
+  }
+
+  try {
+    const assignmentInfo = await AssignmentService.getMaxSubmissionsForAssignment(assignmentId)
+
+    if (assignmentInfo == null) return res.status(403).send('could not retrieve assignment info')
+
+    if (assignmentInfo!.maxSubmissions == null) {
+      console.debug('Max submissions are not specified, skipping check')
+      // check file size
+      if (req.file && req.file.size > assignmentInfo!.maxFileSize!) {
+        return res.status(403).json({
+          'error': 'file is bigger than allowed max file size',
+          'allowed size': assignmentInfo!.maxFileSize!,
+          'file size': req.file.size,
+        })
+      }
+
+      return next()
+    }
+
+    const submissions = await SubmissionService.listByAssignment(assignmentId, userID)
+    // check submissions
+    if (submissions.length >= assignmentInfo!.maxSubmissions!) {
+      return res.status(403).json({
+        'error': 'max submissions reached.',
+        'Max submissions': assignmentInfo!.maxSubmissions!,
+        'Current submissions': submissions.length,
+      })
+    }
+
+    // check file size
+    if (req.file && req.file.size > assignmentInfo!.maxFileSize!) {
+      return res.status(403).json({
+        'error': 'file is bigger than allowed max file size',
+        'allowed size': assignmentInfo!.maxFileSize!,
+        'file size': req.file.size,
+      })
+    }
+
+    next()
+  } catch (e) {
+    return res.status(500).send(e)
+  }
+}
+
+export { checkSubmissions }
