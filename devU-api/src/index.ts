51a84669dabf25a77bf8cf42ec2ea9631b80753d
RA341 <razerishi1505@gmail.com>
2025-02-01 17:26:39 -0500

    streamlined init code

--- devU-api/src/index.ts
+++ devU-api/src/index.ts
@@ -1,6 +1,4 @@
 // Libraries
-import 'reflect-metadata'
-
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
@@ -13,46 +11,53 @@ import { dataSource } from './database'
 import environment from './environment'
 import { initializeMinio } from './fileStorage'
 
+import { initLeviathan } from './autograder/leviathan/leviathan'
+
 // Middleware
 import router from './router'
 import errorHandler from './middleware/errorHandler.middleware'
 
 // Authentication Handlers
 import './utils/passport.utils'
-import { responseInterceptor } from './entities/webhooks/webhooks.middleware'
+import { webhookInterceptor } from './entities/webhooks/webhooks.middleware'
+import { initTango } from './autograder/tango/tango.service'
 
-const app = express()
 
-initializeMinio()
-  .then(() =>
-    dataSource.initialize()
-      .then(() => {
-        console.log('Data Source has been initialized!')
-      })
-      .catch(err => {
-        console.error('Error during Data Source initialization', err)
-      })
+async function main() {
+  // file storage
+  await initializeMinio()
+  // database - typeorm
+  await dataSource.initialize()
+
+  await initLeviathan()
+  await initTango()
+
+  const app = express()
+  // middleware stuff
+  app.use(helmet())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(cookieParser())
+  app.use(cors({ origin: environment.clientUrl, credentials: true }))
+  app.use(morgan('combined'))
+  app.use(passport.initialize())
+  app.use(webhookInterceptor)
+
+  console.log(`DevU api is ${environment.isDocker ? '' : 'not'} running in docker`)
+
+  // main routes
+  app.use('/', router)
+  app.use(errorHandler)
+
+  app.listen(environment.port, () =>
+    console.log(`API listening at port - ${environment.port}\n
+    If you are running the full app, the front end should be accessible at http://localhost:9000`),
   )
-  .then(_connection => {
-    app.use(helmet())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.json())
-    app.use(cookieParser())
-    app.use(cors({ origin: environment.clientUrl, credentials: true }))
-    app.use(morgan('combined'))
-    app.use(passport.initialize())
+}
 
-    console.log(`Api: ${environment.isDocker ? '' : 'not'} running in docker`)
-
-    app.use(responseInterceptor)
-
-    // Middleware;
-    app.use('/', router)
-    app.use(errorHandler)
-
-    app.listen(environment.port, () =>
-      console.log(`API listening at port - ${environment.port}\n
-    If you are running the full app, the front end should be accessible at http://localhost:9000`)
-    )
-  })
-  .catch(err => console.log('TypeORM connection error:', err))
+main().then(_ => {
+  console.log('DevU initialized')
+}).catch(error => {
+  console.log('An error occurred while initializing devU')
+  console.error(error)
+})
